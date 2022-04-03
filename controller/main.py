812f7ec514e5f0d2926660a784207c8adfb0cb79
Quentin Wright <wrightq00@gmail.com>
2022-04-02 21:51:49 -0500

    remove threading

--- controller/main.py
+++ controller/main.py
@@ -1,19 +1,13 @@
-from queue import Queue
-from threading import Thread
 from time import sleep
-
-from input import input_main
+from input import Input
 
 def main():
-	to_input_queue = Queue()
-	from_input_queue = Queue()
-
-	input_thread = Thread(target=input_main, args=(to_input_queue, from_input_queue))
-	input_thread.start()
+	controller = Input()
 
 	while True:
-		sleep(1)
-		to_input_queue.put("test1")
+		print(controller.get_xbox_state())
+		sleep(0.25)
 
 if __name__ == "__main__":
 	main()
+	
